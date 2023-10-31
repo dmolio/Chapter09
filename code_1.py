5a6fd161208321e58606be68a10262a7456b1a0f
dmolio <dmolio@molnafsystems.com>
2023-10-31 13:33:54 -0400

    uploading the scaffold of the project

---
+++ code_1.py
@@ -0,0 +1,33 @@
+# define a function for addition
+def add(num1, num2):
+    return num1 + num2
+
+# define a function for subtraction
+def subtract(num1, num2):
+    return num1 - num2
+
+# define a function for multiplication
+def multiply(num1, num2):
+    return num1 * num2
+
+# define a function for division
+def divide(num1, num2):
+    return num1 / num2
+
+# get user input for operation choice and numbers
+operation = input("Choose an operation (+, -, *, /): ")
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# perform the chosen operation and print the result
+if operation == "+":
+    print(num1, "+", num2, "=", add(num1, num2))
+elif operation == "-":
+    print(num1, "-", num2, "=", subtract(num1, num2))
+elif operation == "*":
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif operation == "/":
+    print(num1, "/", num2, "=", divide(num1, num2))
+else:
+    print("Invalid operation")
+
